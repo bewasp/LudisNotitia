fd94213bf1655dce8a214dd789ea1cc9304d1f7c
bewasp <beewasp1997@gmail.com>
2020-06-03 18:42:35 +0200

    final

--- src/screens/TabScreen.js
+++ src/screens/TabScreen.js
@@ -1,5 +1,14 @@
-import React, { Component } from 'react';
-import { Container, Header, Left, Body, Right, Title, Tab, Tabs } from 'native-base';
+import React, {Component} from 'react';
+import {
+    Container,
+    Header,
+    Left,
+    Body,
+    Right,
+    Title,
+    Tab,
+    Tabs,
+} from 'native-base';
 import Tab1 from './tabs/tab1';
 import Tab2 from './tabs/tab2';
 import Tab3 from './tabs/tab3';
@@ -8,30 +17,33 @@ export default class TabsExample extends Component {
     render() {
         return (
             <Container>
-                <Header style={{backgroundColor:'#000000'}} hasTabs>
-                    <Left/>
-                      <Body>
-                         <Title>Ludis Notitia</Title>
-                      </Body>
+                <Header style={{backgroundColor: '#e2ab24'}} hasTabs>
+                    <Left />
+                    <Body>
+                        <Title>Ludis Notitia</Title>
+                    </Body>
                     <Right />
                 </Header>
-                <Tabs tabBarUnderlineStyle={{backgroundColor:'white'}}>
-                    <Tab tabStyle={{backgroundColor:'#808080'}}
-                         activeTabStyle={{backgroundColor:'#000000'}}
-                         textStyle={{color:'white'}}
-                         heading="Sport news">
+                <Tabs tabBarUnderlineStyle={{backgroundColor: 'white'}}>
+                    <Tab
+                        tabStyle={{backgroundColor: '#c2a87c'}}
+                        activeTabStyle={{backgroundColor: '#eac067'}}
+                        textStyle={{color: 'white'}}
+                        heading="Sport news">
                         <Tab1 />
                     </Tab>
-                    <Tab tabStyle={{backgroundColor:'#808080'}}
-                         activeTabStyle={{backgroundColor:'#000000'}}
-                         textStyle={{color:'white'}}
-                         heading="Standings">
+                    <Tab
+                        tabStyle={{backgroundColor: '#c2a87c'}}
+                        activeTabStyle={{backgroundColor: '#eac067'}}
+                        textStyle={{color: 'white'}}
+                        heading="Standings">
                         <Tab2 />
                     </Tab>
-                    <Tab tabStyle={{backgroundColor:'#808080'}}
-                         activeTabStyle={{backgroundColor:'#000000'}}
-                         textStyle={{color:'white'}}
-                         heading="About us">
+                    <Tab
+                        tabStyle={{backgroundColor: '#c2a87c'}}
+                        activeTabStyle={{backgroundColor: '#eac067'}}
+                        textStyle={{color: 'white'}}
+                        heading="About us">
                         <Tab3 />
                     </Tab>
                 </Tabs>
